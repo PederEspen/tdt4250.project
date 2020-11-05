7bb2b30f3b51e2c9efc9be332a6cb3d6ae562e0a
marcushauge <marcus.hauge1@gmail.com>
2020-11-05 13:40:51 +0100

    added dependencies and new file

---
+++ src/transformations/JsonToModel.java
@@ -0,0 +1,17 @@
+package transformations;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonToModel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void readJson() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		//objectMapper.readValue(json, Car.class);
+	}
+
+}
